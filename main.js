1633abbc4d8559c814e8709864d8b039c6e6af1f
Gootzi <124401935+Gootzi@users.noreply.github.com>
2023-03-13 03:11:50 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+// Get a reference to the container div element
+const container = document.getElementById('my-container');
+
+// Create a new text input element
+const input = document.createElement('input');
+input.type = 'text';
+input.name = 'my-input';
+
+// Add the new input element to the container div element
+container.appendChild(input);
